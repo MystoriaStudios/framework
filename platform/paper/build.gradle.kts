73c2274bdea72a9bb006b9ed54226f2a69b8da4a
Nopox <78834703+Nopock@users.noreply.github.com>
2023-09-20 16:11:29 -0700

    chore(global): add paper api

--- platform/paper/build.gradle.kts
+++ platform/paper/build.gradle.kts
@@ -1,8 +1,3 @@
 repositories {
     maven("https://repo.papermc.io/repository/maven-public/")
 }
-
-dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
-    implementation(project("api"))
-}
--- platform/paper/core/build.gradle.kts
+++ platform/paper/core/build.gradle.kts
@@ -1,2 +1,6 @@
 dependencies {
+    implementation(project(":platform:independent:api"))
+
+    compileOnly("io.papermc.paper:paper-api:1.20.1-R0.1-SNAPSHOT")
+    implementation(project(":platform:paper:api"))
 }
