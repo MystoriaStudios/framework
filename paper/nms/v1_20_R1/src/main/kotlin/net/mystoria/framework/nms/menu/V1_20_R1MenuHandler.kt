6174016e3d4bd382ec1cf6418454e24bfb509e9b
Nopox <78834703+Nopock@users.noreply.github.com>
2023-09-21 22:12:55 -0700

    Update V1_20_R1MenuHandler.kt

--- paper/nms/v1_20_R1/src/main/kotlin/net/mystoria/framework/nms/menu/V1_20_R1MenuHandler.kt
+++ paper/nms/v1_20_R1/src/main/kotlin/net/mystoria/framework/nms/menu/V1_20_R1MenuHandler.kt
@@ -7,6 +7,7 @@ import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket
 import net.minecraft.world.inventory.AbstractContainerMenu
 import net.minecraft.world.inventory.ChestMenu
 import net.minecraft.world.inventory.MenuType
+import org.bukkit.Bukkit
 import org.bukkit.craftbukkit.v1_20_R1.entity.CraftPlayer
 import org.bukkit.craftbukkit.v1_20_R1.event.CraftEventFactory
 import org.bukkit.craftbukkit.v1_20_R1.inventory.CraftContainer
@@ -15,13 +16,16 @@ import org.bukkit.inventory.Inventory
 class V1_20_R1MenuHandler : INMSMenuHandler {
 
     override fun openCustomInventory(p: Any, inventory: Any, size: Int) {
+        Bukkit.broadcastMessage("STARTING OPEN CUSTOM INVENTOORY")
         val player = (p as CraftPlayer).handle
         inventory as Inventory
         Preconditions.checkArgument(true, "Unknown windowType")
         var container: AbstractContainerMenu? = CraftContainer(inventory, player, player.nextContainerCounter())
         val result = CraftEventFactory.callInventoryOpenEventWithTitle(player, container)
         container = result.second
+        Bukkit.broadcastMessage("CHECK 1")
         if (container == null) return
+        Bukkit.broadcastMessage("CHECK 2")
         var component: Component? = container.bukkitView.title()
         if (result.first != null) component = result.first
 
@@ -35,6 +39,7 @@ class V1_20_R1MenuHandler : INMSMenuHandler {
 
         player.containerMenu = container
         player.initMenu(container)
+        Bukkit.broadcastMessage("FINISHING OPEN CUSTOM INVEOOOOOOOOOOOOOOOORY")
     }
 
     private fun getWindowType(size: Int): MenuType<ChestMenu> {
