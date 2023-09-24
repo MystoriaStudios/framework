c0f9691f7ce59439d06b32310ecdacfb33344e6a
Nopox <78834703+Nopock@users.noreply.github.com>
2023-09-24 02:39:01 -0700

    feat(cache): prototype for redis uuid cache

--- independent/api/src/main/kotlin/net/mystoria/framework/cache/impl/distribution/DistributedRedisUUIDCache.kt
+++ independent/api/src/main/kotlin/net/mystoria/framework/cache/impl/distribution/DistributedRedisUUIDCache.kt
@@ -1,19 +1,30 @@
 package net.mystoria.framework.cache.impl.distribution
 
+import io.lettuce.core.ExpireArgs
 import net.mystoria.framework.Framework
 import net.mystoria.framework.cache.IUUIDCacheTranslator
 import net.mystoria.framework.cache.UUIDCacheHelper
 import net.mystoria.framework.connection.redis.impl.BasicFrameworkRedisConnection
 import net.mystoria.framework.flavor.annotation.Inject
+import java.time.Duration
 import java.util.*
 import java.util.concurrent.CompletableFuture
 import java.util.concurrent.CompletionStage
+import kotlin.math.exp
 
 class DistributedRedisUUIDCache : IUUIDCacheTranslator
 {
 
-    @Inject
-    lateinit var redisConnection: BasicFrameworkRedisConnection
+    companion object {
+        const val UUID_CACHE_KEY_UUID = "FRAMEWORK-UUID-CACHE-UUID"
+        const val UUID_CACHE_KEY_USERNAME = "FRAMEWORK-UUID-CACHE-USERNAME"
+    }
+
+    private val redisConnection by lazy {
+        Framework.useWithReturn {
+            it.constructNewRedisConnection()
+        }
+    }
 
     override val uniqueIdCache: MutableMap<UUID, String> = mutableMapOf()
     override val usernameCache: MutableMap<String, UUID> = mutableMapOf()
@@ -27,16 +38,26 @@ class DistributedRedisUUIDCache : IUUIDCacheTranslator
     }
 
     override fun update(response: UUIDCacheHelper.MojangResponse, commit: Boolean) {
-        Framework.use {
+        redisConnection.useResource {
+            val async = this.async()
+            async.hset(UUID_CACHE_KEY_UUID, response.id.toString(), response.name)
+            async.hset(UUID_CACHE_KEY_USERNAME, response.name.lowercase(), response.id.toString())
 
+            // TODO: Expire this
         }
     }
 
     override fun uniqueId(username: String): UUID? {
-        TODO("Not yet implemented")
+        return redisConnection.useResourceWithReturn {
+            this.sync().hget(UUID_CACHE_KEY_USERNAME, username.lowercase()).let {
+                UUID.fromString(it)
+            }
+        }
     }
 
     override fun username(uuid: UUID): String? {
-        TODO("Not yet implemented")
+        return redisConnection.useResourceWithReturn {
+            this.sync().hget(UUID_CACHE_KEY_UUID, uuid.toString())
+        }
     }
 }
