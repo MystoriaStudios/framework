47395a9fa3b87b7e8c9c45a90ef4ab9f8f5004fa
Nopox <78834703+Nopock@users.noreply.github.com>
2023-09-23 20:02:16 -0700

    feat(entities): started on them

--- paper/api/src/main/kotlin/net/mystoria/framework/hotbar/impl/FixedHotbarEntry.kt
+++ paper/api/src/main/kotlin/net/mystoria/framework/hotbar/impl/FixedHotbarEntry.kt
@@ -14,4 +14,7 @@ class FixedHotbarEntry(
 ) : IHotbarEntry {
 
     override val uniqueId: UUID = UUID.randomUUID()
+    override fun getButtonItem(player: Player): ItemStackBuilder.() -> Unit {
+        return builder
+    }
 }
