7e271b22c73e8c7e1ffbc995dc8f8aa0363784b7
Nopox <78834703+Nopock@users.noreply.github.com>
2023-09-24 11:47:52 -0700

    forgog toto push file

--- velocity/api/build.gradle.kts
+++ velocity/api/build.gradle.kts
@@ -1,3 +1,9 @@
 dependencies {
+    implementation(project(":velocity"))
+    implementation(project(":independent:independent-api"))
+
     implementation("com.squareup.retrofit:retrofit:2.0.0-beta2")
+    compileOnly("com.velocitypowered:velocity-api:3.2.0-SNAPSHOT")
+
+    implementation("co.aikar:acf-core:0.5.1-SNAPSHOT")
 }
