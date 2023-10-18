88be7a43263eb106f38ed6a926ff32b16203867d
Nopox <78834703+Nopock@users.noreply.github.com>
2023-10-17 20:54:54 -0700

    fuck physicss class

--- paper/api/src/main/kotlin/net/revive/framework/menu/impl/EditItemStackMenu.kt
+++ paper/api/src/main/kotlin/net/revive/framework/menu/impl/EditItemStackMenu.kt
@@ -3,15 +3,19 @@ package net.revive.framework.menu.impl
 import com.cryptomorin.xseries.XMaterial
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.TextDecoration
+import net.kyori.adventure.text.minimessage.MiniMessage
 import net.revive.framework.constants.Tailwind
 import net.revive.framework.menu.IMenu
 import net.revive.framework.menu.button.IButton
+import net.revive.framework.menu.button.impl.AbstractInputButton
 import net.revive.framework.menu.callback.impl.MaterialCallbackMenu
 import net.revive.framework.menu.openMenu
 import net.revive.framework.menu.paged.AbstractPagedMenu
 import net.revive.framework.utils.ItemStackBuilder
 import net.revive.framework.utils.Tasks
 import net.revive.framework.utils.buildComponent
+import net.wesjd.anvilgui.AnvilGUI
+import org.bukkit.Bukkit
 import org.bukkit.Material
 import org.bukkit.enchantments.Enchantment
 import org.bukkit.entity.Player
@@ -43,8 +47,8 @@ class EditItemStackMenu(private var itemstack: ItemStack? = null) : IMenu {
     )
 
     override fun getButtons(player: Player): Map<Int, IButton> {
-        return mutableMapOf<Int, IButton>().apply {
-            this[4] = object : IButton {
+        return mapOf<Int, IButton>(
+            4 to object : IButton {
                 override fun getMaterial(player: Player) = XMaterial.matchXMaterial(itemstack?.type ?: Material.BARRIER)
 
                 override fun getButtonItem(player: Player): ItemStackBuilder.() -> Unit = {
@@ -73,7 +77,31 @@ class EditItemStackMenu(private var itemstack: ItemStack? = null) : IMenu {
                         player.openMenu(this@EditItemStackMenu)
                     })
                 }
+            },
+
+            5 to object : AbstractInputButton() {
+                override fun builder(player: Player): AnvilGUI.Builder = AnvilGUI.Builder()
+                .plugin(Bukkit.getPluginManager().getPlugin("Framework"))
+                .text("Enter the new item name")
+                .onClick { _, event ->
+                    itemstack?.editMeta {
+                        it.displayName(MiniMessage.miniMessage().deserialize(event.text))
+                    }
+                    player.openMenu(this@EditItemStackMenu)
+                    return@onClick listOf(AnvilGUI.ResponseAction.close())
+                }
+
+                override fun getMaterial(player: Player): XMaterial = XMaterial.OAK_SIGN
+
+                override fun getButtonItem(player: Player): ItemStackBuilder.() -> Unit = {
+                    name(buildComponent {
+                        text("Change Name") {
+                            it.color(Tailwind.AMBER_400)
+                            it.decorate(TextDecoration.BOLD)
+                        }
+                    })
+                }
             }
-        }
+        )
     }
 }
