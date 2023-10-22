ade31d277b86e9ba006bb2341d7ce7587b4e302a
Nopox <78834703+Nopock@users.noreply.github.com>
2023-10-22 13:46:27 -0700

    feat: add tasks

--- minestom/api/src/main/kotlin/net/revive/framework/utils/Tasks.kt
+++ minestom/api/src/main/kotlin/net/revive/framework/utils/Tasks.kt
@@ -1,12 +1,39 @@
 package net.revive.framework.utils
 
 import net.minestom.server.MinecraftServer
+import net.minestom.server.timer.ExecutionType
+import net.minestom.server.timer.Task
+import java.time.Duration
 
 object Tasks {
 
-    val scheduler = MinecraftServer.getSchedulerManager()
+    fun sync(lambda: () -> Unit): Task {
+        return MinecraftServer.getSchedulerManager().buildTask {
+            lambda.invoke()
+        }.executionType(ExecutionType.SYNC).schedule()
+    }
 
-    fun delayed(delay: Long, lambda: () -> Unit) {
-        scheduler.scheduleNextTick(lambda)
+    fun delayed(delay: Duration, lambda: () -> Unit): Task {
+        return MinecraftServer.getSchedulerManager().buildTask {
+            lambda.invoke()
+        }.executionType(ExecutionType.SYNC).delay(delay).schedule()
+    }
+
+    fun timer(delay: Duration, lambda: () -> Unit): Task {
+        return MinecraftServer.getSchedulerManager().buildTask {
+            lambda.invoke()
+        }.executionType(ExecutionType.SYNC).repeat(delay).schedule()
+    }
+
+    fun asyncDelayed(delay: Duration, lambda: () -> Unit): Task {
+        return MinecraftServer.getSchedulerManager().buildTask {
+            lambda.invoke()
+        }.executionType(ExecutionType.ASYNC).delay(delay).schedule()
+    }
+
+    fun asyncTimer(delay: Duration, lambda: () -> Unit): Task {
+        return MinecraftServer.getSchedulerManager().buildTask {
+            lambda.invoke()
+        }.executionType(ExecutionType.ASYNC).repeat(delay).schedule()
     }
 }
