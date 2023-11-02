8669ebc62001ffb0743231cf2bcea4f13e13c92e
Nopox <78834703+Nopock@users.noreply.github.com>
2023-11-01 22:24:37 -0700

    Update NodeConfig.kt

--- paper/core/src/main/kotlin/net/revive/framework/config/NodeConfig.kt
+++ paper/core/src/main/kotlin/net/revive/framework/config/NodeConfig.kt
@@ -2,6 +2,6 @@ package net.revive.framework.config
 
 @JsonConfig("pod.json")
 class NodeConfig(
-    var nodeIP: String = "127.0.0.1",
+    var nodeIP: String = "0.0.0.0",
     var nodePort: Int = 8070
 )
