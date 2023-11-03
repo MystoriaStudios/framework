af27d3991059a5a073301a26fb05153893756e88
98ping <67809373+98ping@users.noreply.github.com>
2023-11-02 21:22:01 -0700

    docs: more docs for DockerRequest#class

--- independent/core/src/main/kotlin/net/revive/framework/deployment/docker/DockerRequest.kt
+++ independent/core/src/main/kotlin/net/revive/framework/deployment/docker/DockerRequest.kt
@@ -11,8 +11,11 @@ import net.revive.framework.deployment.DeploymentService
  * Any other requests just use a constructor to call in
  * values that we can use in the main call method.
  *
- * Not the most clean method of doing this but it will work for what
+ * Not the most clean method of doing this, but it will work for what
  * we need it to.
+ *
+ * Also, we return docker's cancerous version of a response
+ * that for some reason interprets bodies completely raw.
  */
 abstract class DockerRequest(
     val client: DockerHttpClient = DeploymentService.dockerClient
