1e683d82b8032dbf9d524e86e32886d601c0362b
98ping <67809373+98ping@users.noreply.github.com>
2023-11-02 21:18:48 -0700

    docs: document DockerRequest#class

--- independent/core/src/main/kotlin/net/revive/framework/deployment/docker/DockerRequest.kt
+++ independent/core/src/main/kotlin/net/revive/framework/deployment/docker/DockerRequest.kt
@@ -4,11 +4,15 @@ import com.github.dockerjava.transport.DockerHttpClient
 import net.revive.framework.deployment.DeploymentService
 
 /**
- * Class created on 11/2/2023
-
- * @author 98ping
- * @project framework
- * @website https://solo.to/redis
+ * When handling a request, if the request does not need to
+ * include parameters for identification or precise information
+ * about our docker client we make it an object.
+ *
+ * Any other requests just use a constructor to call in
+ * values that we can use in the main call method.
+ *
+ * Not the most clean method of doing this but it will work for what
+ * we need it to.
  */
 abstract class DockerRequest(
     val client: DockerHttpClient = DeploymentService.dockerClient
