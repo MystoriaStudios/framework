ae8f72125a54af2ceeff5033fd2da5ea6e4580d0
98ping <67809373+98ping@users.noreply.github.com>
2023-11-02 21:58:25 -0700

    feat: improve upon deployment templates

--- independent/core/src/main/kotlin/net/revive/framework/deployment/DeploymentService.kt
+++ independent/core/src/main/kotlin/net/revive/framework/deployment/DeploymentService.kt
@@ -4,9 +4,15 @@ import com.github.dockerjava.core.DefaultDockerClientConfig
 import com.github.dockerjava.core.DockerClientConfig
 import com.github.dockerjava.okhttp.OkDockerHttpClient
 import com.github.dockerjava.transport.DockerHttpClient
+import net.revive.framework.FrameworkApp
+import net.revive.framework.config.JsonConfig
+import net.revive.framework.config.load
 import net.revive.framework.deployment.docker.container.ListDockerContainers
+import net.revive.framework.deployment.template.DeploymentTemplate
+import net.revive.framework.flavor.service.Close
 import net.revive.framework.flavor.service.Configure
 import net.revive.framework.flavor.service.Service
+import java.io.File
 
 @Service
 object DeploymentService {
@@ -21,10 +27,33 @@ object DeploymentService {
         .sslConfig(dockerConfig.sslConfig)
         .connectTimeout(30 * 60)
         .readTimeout(45 * 60)
-        .build();
+        .build()
+
+    private val templates: MutableMap<String, DeploymentTemplate> = mutableMapOf()
 
     @Configure
     fun configure() {
+        FrameworkApp.use { app ->
+            val directory = File(app.getBaseFolder(), "persistent")
 
+            if (!directory.exists()) {
+                directory.mkdir()
+            }
+
+            directory.listFiles().forEach { file ->
+                app.load(
+                    JsonConfig(file.name),
+                    DeploymentTemplate::class
+                ).apply {
+                    val template = this as DeploymentTemplate
+                    templates[template.templateKey] = template
+                }
+            }
+        }
+    }
+
+    @Close
+    fun close() {
+        templates.values.forEach(DeploymentTemplate::save)
     }
 }
--- independent/core/src/main/kotlin/net/revive/framework/deployment/template/DeploymentTemplate.kt
+++ independent/core/src/main/kotlin/net/revive/framework/deployment/template/DeploymentTemplate.kt
@@ -1,5 +1,11 @@
 package net.revive.framework.deployment.template
 
+import net.revive.framework.FrameworkApp
+import net.revive.framework.FrameworkNodePlatform
+import net.revive.framework.config.JsonConfig
+import net.revive.framework.config.load
+import net.revive.framework.config.save
+
 /**
  * Class created on 11/2/2023
 
@@ -8,6 +14,14 @@ package net.revive.framework.deployment.template
  * @website https://solo.to/redis
  */
 data class DeploymentTemplate(
-    val nameScheme: String = "example-%containerid%",
-    val idScheme: String = "EXP-%containerId%"
-)
+    val templateKey: String = "example",
+    val nameScheme: String = "example-%containerId%",
+    val idScheme: String = "EXP-%containerId%",
+    val persisted: Boolean = nameScheme.contains("%containerId%"),
+) {
+    fun save() {
+        FrameworkApp.useWithReturn { app ->
+            app.save(JsonConfig("persistent/$nameScheme.json"), this)
+        }
+    }
+}
